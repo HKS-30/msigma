814606e602d93c426d21cd4984422f0a16bdae10
Hema K Shaji <hema.shaji@experionglobal.local>
2023-11-06 22:35:50 +0530

    fix:-added random colour

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+function getRandomColor() {
+  // Generate random values for RGB components
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Create and return the random color in RGB format
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 async function getData() {
   try {
     const result = await fetch("https://api.msigma.in/btech/v2/branches/");
     if (!result.ok) {
-      throw new error(`API request failed ,status :${result.status}`);
+      throw new Error(`API request failed, status: ${result.status}`);
     }
     const data = await result.json();
     console.log(data);
@@ -12,13 +22,20 @@ async function getData() {
       let courseWrapper = document.querySelector(".courseWrapper");
       let courseContainer = document.createElement("div");
       courseContainer.classList.add("courseWrapper");
+
+      // Generate a random color
+      const randomColor = getRandomColor();
+
       let title = document.createElement("h2");
       title.innerHTML = data.short;
+      title.style.color = randomColor; // Apply random color to h2
+
       let description = document.createElement("p");
       description.innerHTML = data.name;
       description.id = "description";
       let button = document.createElement("button");
       button.innerHTML = "Apply now";
+      button.style.border = `1px solid ${randomColor}`; // Apply the same random color to the button border
       let pricing = document.createElement("p");
       pricing.innerHTML = "Fee starting at ₹799 per subject";
       pricing.id = "pricing";
@@ -30,4 +47,5 @@ async function getData() {
     console.log(error);
   }
 }
+
 getData();
